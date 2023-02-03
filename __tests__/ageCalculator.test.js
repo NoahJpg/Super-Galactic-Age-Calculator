56f011f46b11d3a3099563f013b1421bde0e9836
Noah Atkinson <noahatkinson1.1@gmail.com>
2023-02-03 12:16:06 -0800

    WIP: refactor previous age results to return two decimal points after

--- __tests__/ageCalculator.test.js
+++ __tests__/ageCalculator.test.js
@@ -18,23 +18,30 @@ describe('UserData', () => {
   });
 
   test("should correctly return a user's age in Mercury years", () => {
-    expect(user.mercuryAge(user.userAge())).toEqual(104)
+    expect(user.mercuryAge(user.userAge())).toEqual(104.17)
   });
 
   test("should correctly return a user's age in Venus years", () => {
-    expect(user.venusAge(user.userAge())).toEqual(40)
+    expect(user.venusAge(user.userAge())).toEqual(40.32)
   });
 
   test("should correctly return a user's age in Mars years", () => {
-    expect(user.marsAge(user.userAge())).toEqual(13)
+    expect(user.marsAge(user.userAge())).toEqual(13.30)
   });
 
   test("should correctly return a user's age in Jupiter years", () => {
-    expect(user.jupiterAge(user.userAge())).toEqual(2)
+    expect(user.jupiterAge(user.userAge())).toEqual(2.11)
   });
 
   test("should correctly determine how many years have passed on each planet since past birthday", () => {
-    expect(user.yearsPast(user.userAge())).toEqual(5)
+    let yearsPast = user.yearsPast(13);
+    expect(yearsPast).toEqual({
+      "Earth": 13,
+      "Mercury": 54.16,
+      "Venus": 8.06,
+      "Mars": 6.91,
+      "Jupiter": 1.09,
+    })
   });
 
 });
--- src/js/ageCalculator.js
+++ src/js/ageCalculator.js
@@ -8,31 +8,39 @@ export default class UserData {
   userAge() {
     let birthday = new Date(this.year, this.month, this.day);
     let today = Date.now() - birthday;
-    let age = Math.floor(today / (365 * 24 * 60 * 60 * 1000));
-    return age;
+    let age = (today / (365 * 24 * 60 * 60 * 1000));
+    return Math.floor(age)    
   }
 
   mercuryAge(age) {
-    let mercuryAge = Math.floor(age / .24);
-    return mercuryAge;
+    let mercuryAge = (age / .24);
+    return Number(mercuryAge.toFixed(2));
   }
 
   venusAge(age) {
-    let venusAge = Math.floor(age / .62);
-    return venusAge;
+    let venusAge = (age / .62);
+    return Number(venusAge.toFixed(2));
   }
 
   marsAge(age) {
-    let marsAge = Math.floor(age / 1.88);
-    return marsAge;
+    let marsAge = (age / 1.88);
+    return Number(marsAge.toFixed(2));
   }
 
   jupiterAge(age) {
-    let jupiterAge = Math.floor(age / 11.86);
-    return jupiterAge;
+    let jupiterAge = (age / 11.86);
+    return Number(jupiterAge.toFixed(2))
   }
 
-  yearsPast(age) {
- 
+  yearsPast(ageDiff) {
+    let currentAge = this.userAge();
+    let ageDifference = currentAge - ageDiff;
+    return {
+      "Earth": ageDiff,
+      "Mercury": this.mercuryAge(ageDifference.toFixed(2)),
+      "Venus": this.venusAge(ageDifference.toFixed(2)),
+      "Mars": this.marsAge(ageDifference.toFixed(2)),
+      "Jupiter": this.jupiterAge(ageDifference.toFixed(2)),
+    };
   }
 }
